cb6e9bf00db3fda820a66ea0fc53cf1c0f3e90ea
santhosh <ysreddybtech@gmail.com>
2017-04-27 12:16:16 +0530

    changing file

--- test/net/sourceforge/cruisecontrol/sampleproject/connectfour/CellTest.java
+++ test/net/sourceforge/cruisecontrol/sampleproject/connectfour/CellTest.java
@@ -41,8 +41,8 @@ import junit.framework.TestCase;
 public class CellTest extends TestCase {
 
     public void testConstructor() {
-        Cell newCell = new Cell(10, 5);
+        Cell newCell = new Cell(10, 12);
         assertEquals(10, newCell.getColumn());
-        assertEquals(5, newCell.getRow());
+        assertEquals(12, newCell.getRow());
     }
 }
